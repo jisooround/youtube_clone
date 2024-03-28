fa5318ce02a9b4f09354baa155c5bf2891e0ddfb
jisooround <9utesleben@gmail.com>
2024-03-28 16:31:48 +0900

    feat: router 설정

---
+++ src/controllers/videoController.js
@@ -0,0 +1,10 @@
+export const trending = (req, res) => {
+  res.send("Home Page Vidoes");
+};
+
+export const watch = (req, res) => {
+  res.send("Watch Video");
+};
+export const edit = (req, res) => {
+  res.send("Edit Video");
+};
--- src/server.js
+++ src/server.js
@@ -1,40 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouters";
+
 // express 어플리케이션 생성
 const app = express();
 const PORT = 4000;
 
-// get요청
-const handlehome = (req, res) => {
-  // request
-  return res.send("<h1>I still love you</h1>");
-};
-const handleLogin = (req, res) => {
-  console.log("I love middleware");
-  return res.send({ message: "Login here." });
-};
+// 직접 만든 로그 미들웨어
 const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
+// morgan을 사용한 로그 미들웨어
+const loggerMiddleware = morgan("dev");
 
-const handleProteted = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
+app.use(loggerMiddleware);
 
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handlehome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProteted);
+// 라우터
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 // 외부 접속 listen
 const handleListening = () => {
